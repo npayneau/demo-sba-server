37f42f029b8a7e529169fb26069107c177cc46eb
Nicolas Payneau <nicolas.payneau@gmail.com>
2017-06-26 16:52:07 +0200

    Add demo

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# demo_sba_server
-Démo de Spring Boot Admin serveur
+# Demo Spring Boot Admin
+
+Démo de mise en place d'un Spring Boot Admin Server
+
+## Pré-requis
+
+Disposer d'un JDK8
+
+## Compilation
+
+Lancer la commande suivante:
+
+```
+mvn clean install
+```
+
+## Execution
+
+Lancer la commande suivante:
+
+```
+java -jar core/target/core.jar
+```
+
+Utiliser l'URL suivante pour accèder à votre console d'administration
---
+++ core/src/main/java/org/example/nicop/demo_sba_server/Application.java
@@ -0,0 +1,27 @@
+package org.example.nicop.demo_sba_server;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+/* l'exclusion évite que les Repositories soient exposés automatiquement avec Spring data Rest. */
+@EnableAutoConfiguration(exclude = RepositoryRestMvcAutoConfiguration.class)
+@EnableAdminServer
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
